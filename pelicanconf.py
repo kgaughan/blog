72dab0ca81205ed0abdfe781bbdecd53a3bc4533
Keith Gaughan <k@stereochro.me>
2020-07-13 22:17:34 +0100

    Set a favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -90,9 +90,11 @@ DOCUTILS_SETTINGS = {
 STATIC_PATHS = [
     "images",
     "extra/robots.txt",
+    "extra/favicon.ico",
 ]
 EXTRA_PATH_METADATA = {
     "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
 }
 # fmt: on
 
