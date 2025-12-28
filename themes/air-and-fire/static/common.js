d6872237a945383698cfa39d4199df936299c953
Keith Gaughan <k@stereochro.me>
2025-12-28 20:28:46 +0000

    Replace luxon with a more lightweight option

--- themes/air-and-fire/static/common.js
+++ themes/air-and-fire/static/common.js
@@ -1,9 +1,8 @@
 // Humanise any timestamps
 window.addEventListener("DOMContentLoaded", () => {
-	let humanise = (dt) => luxon.DateTime.fromISO(dt).toRelative();
 	document.querySelectorAll("time").forEach((elem, _) => {
 		if (elem.dateTime != "") {
-			elem.innerText = humanise(elem.dateTime);
+			elem.innerText = dayjs(elem.dateTime).fromNow();
 		}
 	});
 });
